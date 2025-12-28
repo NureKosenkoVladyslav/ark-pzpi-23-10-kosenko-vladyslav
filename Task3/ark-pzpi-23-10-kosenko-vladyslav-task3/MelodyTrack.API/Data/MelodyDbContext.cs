98970fe2767cb79a21cb9c4768e033620f514a58
NureKosenkoVladyslav <vladyslav.kosenko@nure.ua>
2025-12-28 13:29:09 +0200

    Add files via upload

---
+++ Task3/ark-pzpi-23-10-kosenko-vladyslav-task3/MelodyTrack.API/Data/MelodyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MelodyTrack.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MelodyTrack.API.Data
+{
+    public class MelodyDbContext : DbContext
+    {
+        public MelodyDbContext(DbContextOptions<MelodyDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; } 
+        public DbSet<Repertoire> Repertoire { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+    }
+}
